5334a77d1f1a26c5e8977a904c6126cb2595dedc
Lauren Days <lmchambers0420@gmail.com>
2018-10-07 14:22:54 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,1 +1,89 @@
+# Helper Method
 
+WIN_COMBINATIONS = [
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [0,4,8],
+  [2,4,6]
+  
+  ]
+  
+  board = [" "," "," "," "," "," "," "," "," ",]
+  
+  def display_board(board)
+    puts " #{board[0]} | #{board[1]} | #{board[2]} "
+    puts "-----------"
+    puts " #{board[3]} | #{board[4]} | #{board[5]} "
+    puts "-----------"
+    puts " #{board[6]} | #{board[7]} | #{board[8]} "
+  end
+  
+  def input_to_index(input)
+    input.to_i - 1
+  end
+  
+  def player_move(board, position, token)
+    index = position -1
+    board[index] = token
+  end
+  
+  def position_taken?(board, index)
+    !(board[index] == " ")
+  end
+  
+  def valid_move?(board, index)
+    index.between?(0,8) && !position_taken?(board, index)
+  end
+  
+  def turn(board) 
+  puts "Please enter 1-9:"
+  position = gets.strip
+  index = input_to_index(position)
+  if valid_move?(board, index)
+    token = current_player(board)
+    player_move(board, position, token)
+    display_board(board)
+  else
+    turn(board)
+  end
+end
+
+def turn_count(board)
+  9 - board.count(" ")
+end
+
+def current_player(board)
+  if turn_count(board).even?
+    "X"
+  else
+    "O"
+  end
+end
+
+def won?
+  
+end
+
+def full?
+  
+end
+
+def draw?
+  
+end
+
+def over?
+  
+end
+
+def winner
+  
+end
+
+def play
+  
+end
