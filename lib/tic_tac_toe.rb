01123c76006a04b1f34073db452a5fa2202765b2
Lauren Days <lmchambers0420@gmail.com>
2018-10-07 14:56:53 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -118,4 +118,13 @@ def winner(board)
 end
 
 def play(board)
-  
+  until over?(board) == true
+    turn(board)
+  end
+
+  if won?(board)
+    puts "Congratulations #{winner(board)}!"
+  elsif draw?(board)
+    puts "Cats Game!"
+  end
+end
